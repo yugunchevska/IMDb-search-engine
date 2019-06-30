d29e0f25ea2a1334072c0fb8bb443c4c182a0ba9
Yuliana Gunchevska <yuliana.gunchevska@gmail.com>
2019-06-30 22:09:28 +0300

    Working with relative paths

--- src/test/java/bg/test/OMDbManagerTest.java
+++ src/test/java/bg/test/OMDbManagerTest.java
@@ -1,66 +1,59 @@
 package bg.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 import bg.uni.sofia.fmi.server.OMDbManager;
+import bg.uni.sofia.fmi.server.util.OMDbManagerConstants;
 
 import java.io.File;
 import java.io.IOException;
 
-public class ExtractionTest {
+public class OMDbManagerTest {
 
 	@Test
-	public void getMovieTest() throws IOException {
-		OMDbManager extr = new OMDbManager();
+	public void testGetMovie() throws IOException {
+		OMDbManager omdbManager = new OMDbManager();
 		String[] result = "get-movie Titanic --fields= Year".split(" ");
-		String expect = "D:" + File.separator + "”ÌË" + File.separator + "Eclipse new" + File.separator + "Project vol.2"
-				+ File.separator + "Titanic  Year .txt";
-		assertEquals("get-movie command doesn't work", expect, extr.getMovie(result));
+		String relativeMovieInfoPath = OMDbManagerConstants.MOVIE_INFO_PATH + File.separator + "Titanic  Year .txt";
+		File expectedMovieInfoFile = new File(relativeMovieInfoPath);
+		assertEquals("get-movie command doesn't work", expectedMovieInfoFile.getAbsolutePath(), omdbManager.getMovie(result));
 	}
 
 	@Test
-	public void getTvSeriesTest() {
-		OMDbManager extr = new OMDbManager();
+	public void testGetTVSeries() throws IOException {
+		OMDbManager omdbManager = new OMDbManager();
 		String[] result = "get-tv-series Friends --season= 7".split(" ");
-		String expect = "D:" + File.separator + "”ÌË" + File.separator + "Eclipse new" + File.separator + "Project vol.2"
-				+ File.separator + "Friends  season 7.txt";
-		assertEquals("get-tv-series command doesn't work", expect, extr.getTVSeries(result));
+		String relativeMovieInfoPath = OMDbManagerConstants.MOVIE_INFO_PATH + File.separator + "Friends  season 7.txt";
+		File expectedMovieInfoFile = new File(relativeMovieInfoPath);
+		assertEquals("get-tv-series command doesn't work", expectedMovieInfoFile.getAbsolutePath(), omdbManager.getTVSeries(result));
 	}
 
 	@Test
-	public void getMoviesTest() {
-		OMDbManager extr = new OMDbManager();
+	public void testGetMovies() throws IOException {
+		OMDbManager omdbManager = new OMDbManager();
 		String[] result = "get-movies --order= desc --actors= Leonardo DiCaprio, Kate Winslet --genres= Drama, Romance"
 				.split(" ");
-		String expect = "D:" + File.separator + "”ÌË" + File.separator + "Eclipse new" + File.separator + "Project vol.2"
-				+ File.separator + "Movies with Leonardo Kate.txt";
-		assertEquals("get-movies command doesn't work", expect, extr.getMovies(result));
+		String relativeMovieInfoPath = OMDbManagerConstants.MOVIE_INFO_PATH + File.separator + "Movies with Leonardo Kate.txt";
+		File expectedMovieInfoFile = new File(relativeMovieInfoPath);
+		assertEquals("get-movies command doesn't work", expectedMovieInfoFile.getAbsolutePath(), omdbManager.getMovies(result));
 	}
 
 	@Test
-	public void getPosterTest() {
-		OMDbManager extr = new OMDbManager();
+	public void testGetPoster() throws IOException {
+		OMDbManager omdbManager = new OMDbManager();
 		String[] result = "get-movie-poster Oblivion".split(" ");
-		String expect = "Oblivion .jpg";
-		assertEquals("get-tv-poster command doesn't work", expect, extr.getPoster(result));
+		String expectedMoviePosterFilename = "Oblivion .jpg";
+		assertEquals("get-tv-poster command doesn't work", expectedMoviePosterFilename, omdbManager.getPoster(result));
 	}
 
 	@Test
-	public void isIncorrectMovie() {
-		OMDbManager extr = new OMDbManager();
+	public void testWithIncorrectMovie() throws IOException {
+		OMDbManager omdbManager = new OMDbManager();
 		String[] result = "get-movie-poster Titani".split(" ");
-		String expect = "Movie not found.";
-		assertEquals("get-tv-poster command doesn't work", expect, extr.getPoster(result));
-	}
-
-	@Test
-	public void findPosterURL() {
-		OMDbManager extr = new OMDbManager();
-		String inputLine = "\"Language\":\"English, French\",\"Country\":\"USA\",\"Awards\":\"4 wins & 9 nominations.\",\"Poster\":\"https://images-na.ssl-images-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg\"";
-		String expect = "https://images-na.ssl-images-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg";
-		assertEquals("can't find poster URL", expect, extr.findPosterURL(inputLine));
+		String errorMessage = "Movie not found.";
+		assertEquals("get-tv-poster command doesn't work", errorMessage, omdbManager.getPoster(result));
 	}
 
 }
